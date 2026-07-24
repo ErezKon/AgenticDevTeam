59797b7c945e2b8eb14c28c2183e0367ebe1f0cd
ErezKon <erezkon@gmail.com>
2026-07-24 12:42:02 +0300

    Minor manual calculator fixes

--- generated-projects/initialcalculator/src/main.tsx
+++ generated-projects/initialcalculator/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
+import CalculatorApp from './CalculatorApp';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <CalculatorApp />
   </React.StrictMode>
 );
